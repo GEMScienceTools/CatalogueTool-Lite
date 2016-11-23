d19f5b4c937bd2d744c5c6f5060659639adc98f3
vpoggi <valerio.poggi@globalquakemodel.org>
2016-11-23 17:43:26 +0100

    Global Magnitude Rules & Time Plot

--- libs/Exploration.py
+++ libs/Exploration.py
@@ -17,259 +17,49 @@
 #
 # Author: Poggi Valerio
 
-import copy as cp
-import math as ma
-
-import Catalogue as Cat
-import CatUtils as CU
+import matplotlib.pyplot as plt
+import numpy as np
 
 #-----------------------------------------------------------------------------------------
 
-def AreaSelect(Db, XY, File=[], Owrite=False):
+def GetHypocenter(Db):
 
-  P = CU.Polygon()
+  x = Db.Extract('Longitude')
+  y = Db.Extract('Latitude')
+  z = Db.Extract('Depth')
 
-  if File:
-    P.Import(XY, Type=File)
-  else:
-    P.Load(XY)
+  return x, y, z
 
-  DbC = Cat.Database()
-  DbC.Header = cp.deepcopy(Db.Header)
+#-----------------------------------------------------------------------------------------
+
+def GetMagnitudePair(Db, Code1, Code2):
+
+  Mout = [[],[],[],[]]
 
   for E in Db.Events:
+    m1 = None
+    m2 = None
 
-    Event = {}
-    Event['Id'] = E['Id']
-    Event['Location'] = []
-    Event['Magnitude'] = E['Magnitude']
-    Event['Log'] = E['Log']
+    for M in E['Magnitude']:
+      MC = M['MagCode']
+      MT = M['MagType']
+      MS = M['MagSize']
+      ME = M['MagError']
 
-    for L in E['Location']:
+      if MC == Code1[0] and MT == Code1[1]:
+        m1 = MS
+        e1 = ME
+      if MC == Code2[0] and MT == Code2[1]:
+        m2 = MS
+        e2 = ME
 
-      x = L['Longitude'];
-      y = L['Latitude']
+    if m1 and m2:
+      Mout[0].append(m1)
+      Mout[1].append(m2)
+      Mout[2].append(e1)
+      Mout[3].append(e2)
 
-      if P.IsInside(x,y):
-        Event['Location'].append(L)
-
-    if Event['Location']:
-      DbC.Events.append(cp.deepcopy(Event))
-
-  if Owrite:
-    Db.Events = DbC.Events
-  else:
-    return DbC
-
-#-----------------------------------------------------------------------------------------
-
-def MagRangeSelect(Db, MinMag, MaxMag, Owrite=False):
-
-  if Owrite:
-    DbC = Db
-  else:
-    DbC = Db.Copy()
-
-  DbC.Filter('MagSize',MinMag,Opr='>=')
-  DbC.Filter('MagSize',MaxMag,Opr='<')
-
-  if not Owrite:
-    return DbC
-
-#-----------------------------------------------------------------------------------------
-
-def DepRangeSelect(Db, MinDep, MaxDep, Owrite=False):
-
-  if Owrite:
-    DbC = Db
-  else:
-    DbC = Db.Copy()
-
-  DbC.Filter('Depth',MinDep,Opr='>=')
-  DbC.Filter('Depth',MaxDep,Opr='<')
-
-  if not Owrite:
-    return DbC
-
-#-----------------------------------------------------------------------------------------
-
-def MagCodeSelect(Db, MagList, Best=False, Owrite=False):
-
-  if type(MagList) != list:
-    MagList = [MagList]
-
-  Code = [M[0] for M in MagList]
-  Type = [M[1:] for M in MagList]
-
-  Db0 = Cat.Database()
-  Db0.Header = cp.deepcopy(Db.Header)
-
-  Db1 = Cat.Database()
-  Db1.Header = cp.deepcopy(Db.Header)
-
-  for E in Db.Events:
-
-    Event = {}
-    Event['Id'] = E['Id']
-    Event['Log'] = E['Log']
-    Event['Location'] = E['Location']
-    Event['Magnitude'] = []
-    Stop = False
-
-    for I,C in enumerate(Code):
-      for M in E['Magnitude']:
-        if C == M['MagCode']:
-          for T in Type[I]:
-            if T == M['MagType']:
-
-              Event['Magnitude'].append(M)
-              if Best:
-                Stop = True
-
-            if Stop: break
-        if Stop: break
-      if Stop: break
-
-    if Event['Magnitude']:
-      Db0.Events.append(cp.deepcopy(Event))
-    else:
-      Db1.Events.append(cp.deepcopy(E))
-
-  if Owrite:
-    Db.Events = Db0.Events
-  else:
-    return Db0, Db1
-
-#-----------------------------------------------------------------------------------------
-
-def LocCodeSelect(Db, LocList, Best=False, Owrite=False):
-
-  if type(LocList) != list:
-    LocList = [LocList]
-
-  Db0 = Cat.Database()
-  Db0.Header = cp.deepcopy(Db.Header)
-
-  Db1 = Cat.Database()
-  Db1.Header = cp.deepcopy(Db.Header)
-
-  for E in Db.Events:
-
-    Event = {}
-    Event['Id'] = E['Id']
-    Event['Log'] = E['Log']
-    Event['Location'] = []
-    Event['Magnitude'] = E['Magnitude']
-    Stop = False
-
-    for C in LocList:
-      for L in E['Location']:
-        if C == L['LocCode']:
-
-          Event['Location'].append(L)
-          if Best:
-           Stop = True
-
-        if Stop: break
-      if Stop: break
-
-    if Event['Location']:
-      Db0.Events.append(cp.deepcopy(Event))
-    else:
-      Db1.Events.append(cp.deepcopy(E))
-
-  if Owrite:
-    Db.Events = Db0.Events
-  else:
-    return Db0, Db1
-
-#-----------------------------------------------------------------------------------------
-
-def TimeSelect(Db, Date0, Date1, Owrite=False):
-
-  Def0 = [1900,1,1,0,0,0]
-  Def1 = [2000,12,31,23,59,59]
-
-  if type(Date0) != list:
-    Date0 = [Date0]
-  if type(Date1) != list:
-    Date1 = [Date1]
-
-  for N in range(len(Date0),6):
-    Date0.append(Def0[N])
-  for N in range(len(Date1),6):
-    Date1.append(Def1[N])
-
-  Sec0 = CU.DateToSec(Date0[0],
-                      Date0[1],
-                      Date0[2],
-                      Date0[3],
-                      Date0[4],
-                      Date0[5])
-
-  Sec1 = CU.DateToSec(Date1[0],
-                      Date1[1],
-                      Date1[2],
-                      Date1[3],
-                      Date1[4],
-                      Date1[5])
-
-  def GetSec(Event):
-    L = Event['Location'][0]
-    S = CU.DateToSec(L['Year'],
-                     L['Month'],
-                     L['Day'],
-                     L['Hour'],
-                     L['Minute'],
-                     L['Second'])
-    return S
-
-  Sec = []
-  for E in Db.Events:
-    Sec.append(GetSec(E))
-
-  Ind = [I for I,S in enumerate(Sec) if S>=Sec0 and S<=Sec1]
-
-  DbN = Cat.Database()
-  DbN.Header = cp.deepcopy(Db.Header)
-
-  for I in Ind:
-    DbN.Events.append(cp.deepcopy(Db.Events[I]))
-
-  if Owrite:
-    Db.Events = DbN.Events
-  else:
-    return DbN
-
-#-----------------------------------------------------------------------------------------
-
-def SplitPrime(Db):
-
-  OutP = Cat.Database()
-  OutN = Cat.Database()
-
-  OutP.Header = cp.deepcopy(Db.Header)
-  OutN.Header = cp.deepcopy(Db.Header)
-
-  for E in Db.Events:
-
-    Event = {}
-    Event['Id'] = E['Id']
-    Event['Magnitude'] = E['Magnitude']
-    Event['Location'] = []
-    Event['Log'] = E['Log']
-
-    for L in E['Location']:
-      if L['Prime']:
-        Event['Location'].append(L)
-
-    if Event['Location']:
-      OutP.Events.append(cp.deepcopy(Event))
-    else:
-      Event['Location'] = E['Location']
-      OutN.Events.append(cp.deepcopy(Event))
-
-  return OutP, OutN
+  return Mout
 
 #-----------------------------------------------------------------------------------------
 
@@ -330,163 +120,62 @@ def AgencyReport(Db, Code, Key=[], LogFile=[], Threshold=0):
 
 #-----------------------------------------------------------------------------------------
 
-def GetHypocenter(Db):
+def TimePlot(Db, Code, Key=[], Year0=[], Year1=[], Delta=5, Threshold=50):
 
-  x = Db.Extract('Longitude')
-  y = Db.Extract('Latitude')
-  z = Db.Extract('Depth')
+  if not Year0:
+    Year0 = min(Db.Extract('Year'))
 
-  return x, y, z
+  if not Year1:
+    Year1 = max(Db.Extract('Year'))
 
-#-----------------------------------------------------------------------------------------
+  YBins = np.arange(Year0, Year1+Delta, Delta)
 
-def GetMagnitudePair(Db, Code1, Code2):
+  ItL, ItD = Db.Occurrence(Code)
 
-  Mout = [[],[],[],[]]
+  # Filter by threshold
+  ItL = [K for K in ItL if ItD[K] > Threshold]
+  ItD = {K:V for (K,V) in ItD.items() if V > Threshold}
 
-  for E in Db.Events:
-    m1 = None
-    m2 = None
+  # Filter by key
+  if Key:
+    ItL = [K for K in ItL if K in Key]
+    ItD = {K:ItD[K] for K in ItL}
 
-    for M in E['Magnitude']:
-      MC = M['MagCode']
-      MT = M['MagType']
-      MS = M['MagSize']
-      ME = M['MagError']
+  for N, Agn in enumerate(ItL):
 
-      if MC == Code1[0] and MT == Code1[1]:
-        m1 = MS
-        e1 = ME
-      if MC == Code2[0] and MT == Code2[1]:
-        m2 = MS
-        e2 = ME
+    DbA = Db.Filter(Code, Agn, Owrite=0)
+    YearArray = DbA.Extract('Year')
+    NewRow = np.histogram(YearArray, YBins)
 
-    if m1 and m2:
-      Mout[0].append(m1)
-      Mout[1].append(m2)
-      Mout[2].append(e1)
-      Mout[3].append(e2)
-
-  return Mout
-
-#-----------------------------------------------------------------------------------------
-
-def MergeDuplicate(DbA, DbB=[],
-                        Twin=60.,
-                        Swin=50.,
-                        Owrite=True,
-                        Log=False):
-
-  def GetDate(Event):
-
-    L = Event['Location'][0]
-    S = CU.DateToSec(L['Year'],
-                     L['Month'],
-                     L['Day'],
-                     L['Hour'],
-                     L['Minute'],
-                     L['Second'])
-    return S
-
-  def GetCoor(Event):
-    L = Event['Location'][0]
-    X = L['Longitude']
-    Y = L['Latitude']
-    return [X, Y]
-
-  def DeltaSec(S0, S1):
-    Sec = ma.fabs(S1-S0)
-    return Sec
-
-  def DeltaLen(C0, C1):
-    Dis = CU.WgsDistance(C0[1],C0[0],C1[1],C1[0])
-    return Dis
-
-  Db0 = DbA.Copy()
-  Db1 = DbB.Copy() if DbB else DbA.Copy()
-  End = None if DbB else -1
-  LogE = []
-  Ind = []
-  Name = Db1.Header['Name']
-
-  for J, E0 in enumerate(Db0.Events[:End]):
-    T0 = GetDate(E0)
-    C0 = GetCoor(E0)
-
-    Start = 0 if DbB else J+1
-
-    for I, E1 in enumerate(Db1.Events[Start:]):
-      C1 = GetCoor(E1)
-      dC = DeltaLen(C0, C1)
-
-      if (dC <= Swin):
-        T1 = GetDate(E1)
-        dT = DeltaSec(T0, T1)
-
-        if (dT <= Twin):
-          E0['Location'].extend(E1['Location'])
-          E0['Magnitude'].extend(E1['Magnitude'])
-          E0['Log'] += 'MERG({0},{1});'.format(Name,E1['Id'])
-          LogE.append((J, E0['Id'], Start+I, E1['Id'], dT, dC))
-          Ind.append(Start+I)
-
-  if DbB:
-    for I in range(0,Db1.Size()):
-      if I not in Ind:
-        Db0.Events.append(Db1.Events[I])
-
-  else:
-    for I in Ind:
-      Db0.Events[I] = []
-    Db0.Events = [e for e in Db0.Events if e]
-
-  if Owrite:
-    DbA.Events = Db0.Events
-    if Log:
-      return LogE
-
-  else:
-    if Log:
-      return Db0, LogE
+    if N == 0:
+      Histo = NewRow[0]
     else:
-      return Db0
+      Histo = np.vstack([Histo, NewRow[0]])
 
-#-----------------------------------------------------------------------------------------
+  # Plot time histogram
+  fig = plt.figure(figsize=(8, 5))
 
-def MagConvert(Db, MagAgency, MagOld, MagNew, ConvFun, Owrite=True):
+  X = YBins
+  Y = np.arange(0, len(ItL)+1)
+  Z = np.log(Histo.clip(min=1E-10))
 
-  if type(MagOld) != list:
-    MagOld = [MagOld]
+  plt.pcolor(X, Y, Z, cmap='Purples',
+                      vmin=0,
+                      vmax=np.max(Z))
 
-  if type(MagOld) != list:
-    MagOld = [MagOld]
+  plt.xticks(X,map(str,X), rotation='45')
+  plt.yticks(Y+0.5, ItL, rotation='horizontal')
+  plt.margins(0)
 
-  if Owrite:
-    DbC = Db
-  else:
-    DbC = Db.Copy()
+  plt.gca().yaxis.tick_right()
+  plt.axes().yaxis.grid(True)
 
-  for E in DbC.Events:
-    for A in E['Magnitude']:
+  plt.gca().xaxis.set_ticks_position('none')
+  plt.gca().yaxis.set_ticks_position('none')
 
-      for Agn in MagAgency:
-        if A['MagCode'] == Agn or Agn == '*':
+  plt.xlabel('Year', fontsize=14, fontweight='bold')
+  plt.ylabel('Agency Code', fontsize=14, fontweight='bold')
 
-          for Mag in MagOld:
-            if A['MagType'] == Mag or Mag == '*':
+  plt.tight_layout()
 
-              MS = A['MagSize']
-              ME = A['MagError']
-
-              if not MS: MS = None
-              if not ME: ME = 0
-
-              ms,me = ConvFun(MS,ME)
-
-              A['MagSize'] = CU.CastValue('MagSize', ms)
-              A['MagError'] = CU.CastValue('MagError', me)
-              A['MagType'] = CU.CastValue('MagType', MagNew)
-              E['Log'] += 'ORMAG({0},{1});'.format(A['MagCode'],A['MagType'])
-
-  if not Owrite:
-    return DbC
+  plt.show(block=False)
--- libs/MagRules.py
+++ libs/MagRules.py
@@ -20,28 +20,189 @@
 import math as mt
 
 #----------------------------------------------
+# Generic
+
+def Mw_Mw_Generic(MagSize, MagError)
+  """
+  """
+
+  return (MagSize, MagError)
+
+#----------------------------------------------
 # Scordilis (2006)
+#
+# MS >> Mw ------------------------------------
 
-def MsMw_Scordillis2006(MagSize, MagError):
+def Ms_Mw_Scordilis2006(MagSize, MagError):
   """
   """
 
-  if MagSize < 6.1:
+  if MagSize >= 3.0 and MagSize <= 6.1:
     M = 0.67 * MagSize + 2.07
     E = mt.sqrt(0.17**2. + MagError**2.)
 
-  else:
+  elif MagSize > 6.1 and MagSize <= 8.2:
     M = 0.99 * MagSize + 0.08
     E = mt.sqrt(0.2**2. + MagError**2.)
 
+  else:
+    M = None
+    E = None
+
   return (M, E)
 
+# mb >> Mw ------------------------------------
 
-def mbMw_Scordillis2006(MagSize, MagError):
+def mb_Mw_Scordilis2006(MagSize, MagError):
   """
   """
 
-  M = 0.85 * MagSize + 1.03
-  E = mt.sqrt(0.29**2. + MagError**2.)
+  if MagSize >= 3.5 and MagSize <= 6.2:
+    M = 0.85 * MagSize + 1.03
+    E = mt.sqrt(0.29**2. + MagError**2.)
+
+  else:
+    M = None
+    E = None
 
   return (M, E)
+
+#----------------------------------------------
+# Di Giacomo 2015
+#
+# Note:
+#   No Error is provided from the paper.
+#   Magnitude range is taken approximatively 
+#   from the paper's pictures. To check...
+#
+# MS >> Mw ------------------------------------
+
+def Ms_Mw_Lin_DiGiacomo2015(MagSize, MagError):
+  """
+  Piecewise linear
+  """
+
+  if MagSize >= 3.5 and MagSize <= 6.47:
+    M = 0.67 * MagSize + 2.13
+    E = MagError
+
+  elif MagSize > 6.47 and MagSize <= 8.0:
+    M = 1.10 * MagSize - 0.67
+    E = MagError
+
+  else:
+    M = None
+    E = None
+
+  return (M, E)
+
+def Ms_Mw_Exp_DiGiacomo2015(MagSize, MagError):
+  """
+  Exponential
+  """
+
+  if MagSize >= 3.5 and MagSize <= 8.0:
+    M = mt.exp(-0.222 + 0.233 * MagSize) + 2.863
+    E = MagError
+
+  else:
+    M = None
+    E = None
+
+  return (M, E)
+
+# mb >> Mw ------------------------------------
+
+def mb_Mw_Lin_DiGiacomo2015(MagSize, MagError):
+  """
+  Linear
+  """
+
+  if MagSize >= 4.0 and MagSize <= 6.5:
+    M = 1.38 * MagSize - 1.79
+    E = MagError
+
+  else:
+    M = None
+    E = None
+
+  return (M, E)
+
+def mb_Mw_Exp_DiGiacomo2015(MagSize, MagError):
+  """
+  Exponential
+  """
+
+  if MagSize >= 4.0 and MagSize <= 6.5:
+    M =  mt.exp(-4.664 + 0.859 * MagSize) + 4.555
+    E = MagError
+
+  else:
+    M = None
+    E = None
+
+  return (M, E)
+
+#----------------------------------------------
+# Weatherill 2016
+#
+# MS >> Mw ------------------------------------
+
+def Ms_Mw_ISC_Weatherill2016(MagSize, MagError):
+  """
+  Piecewise linear
+  """
+
+  if MagSize >= 3.5 and MagSize <= 6.0:
+    M = 0.616 * MagSize + 2.369
+    E = mt.sqrt(0.147**2. + MagError**2.)
+
+  elif MagSize > 6.0 and MagSize <= 8.0:
+    M = 0.994 * MagSize + 0.1
+    E = mt.sqrt(0.174**2. + MagError**2.)
+
+  else:
+    M = None
+    E = None
+
+  return (M, E)
+
+def Ms_Mw_NEIC_Weatherill2016(MagSize, MagError):
+  """
+  Piecewise linear
+  """
+
+  if MagSize >= 3.5 and MagSize <= 6.47:
+    M = 0.723 * MagSize + 1.798
+    E = mt.sqrt(0.159**2. + MagError**2.)
+
+  elif MagSize > 6.47 and MagSize <= 8.0:
+    M = 1.005 * MagSize - 0.026
+    E = mt.sqrt(0.187**2. + MagError**2.)
+
+  else:
+    M = None
+    E = None
+
+  return (M, E)
+
+def Msz_Mw_NEIC_Weatherill2016(MagSize, MagError):
+  """
+  Piecewise linear
+  """
+
+  if MagSize >= 3.5 and MagSize <= 6.47:
+    M = 0.707 * MagSize + 1.933
+    E = mt.sqrt(0.179**2. + MagError**2.)
+
+  elif MagSize > 6.47 and MagSize <= 8.0:
+    M = 0.950 * MagSize + 0.359
+    E = mt.sqrt(0.204**2. + MagError**2.)
+
+  else:
+    M = None
+    E = None
+
+  return (M, E)
+
+# mb >> Mw ------------------------------------
